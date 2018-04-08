a5b157f9fbde7f3c0ab9a0480a8b5689b0ade794
jislam <engr.jislam@gmail.com>
2018-04-08 23:37:24 +0300

    data off

--- app/src/main/java/com/example/ahsanulhoque/finoffer/MainActivity.java
+++ app/src/main/java/com/example/ahsanulhoque/finoffer/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         merchantDatabaseReference = FirebaseDatabase.getInstance().getReference("merchants");
-        addMerchant();
+        //addMerchant();
     }
 
     void addMerchant() {
