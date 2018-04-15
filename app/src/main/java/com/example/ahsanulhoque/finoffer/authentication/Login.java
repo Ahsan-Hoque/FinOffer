f823be5d0adbdaeb2991986cd351147f95fbed9b
Md Ahsanul Hoque <ahsan.ror@gmail.com>
2018-04-15 15:46:57 +0300

    Signup mockup added

--- app/src/main/java/com/example/ahsanulhoque/finoffer/authentication/Login.java
+++ app/src/main/java/com/example/ahsanulhoque/finoffer/authentication/Login.java
@@ -1,14 +1,30 @@
 package com.example.ahsanulhoque.finoffer.authentication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.ahsanulhoque.finoffer.MainActivity;
 import com.example.ahsanulhoque.finoffer.R;
 
 public class Login extends AppCompatActivity {
+    TextView SignUPTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        SignUPTV = (TextView) findViewById(R.id.SignUPTV);
+        SignUPTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Signup.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
