cca5a41b9c99ddfc4f589f6b006091334b1d9679
Md Ahsanul Hoque <ahsan.ror@gmail.com>
2018-04-15 14:29:49 +0300

    Login screen complete

---
+++ app/src/main/java/com/example/ahsanulhoque/finoffer/authentication/Login.java
@@ -0,0 +1,14 @@
+package com.example.ahsanulhoque.finoffer.authentication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.ahsanulhoque.finoffer.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
---
+++ app/src/main/java/com/example/ahsanulhoque/finoffer/authentication/Signup.java
@@ -0,0 +1,13 @@
+package com.example.ahsanulhoque.finoffer.authentication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.ahsanulhoque.finoffer.R;
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+    }
+}
