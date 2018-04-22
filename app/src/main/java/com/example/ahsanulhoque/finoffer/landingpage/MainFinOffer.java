63a5d9000c9bd6bf719c99bd44ee21b4caa42fe1
Md Ahsanul Hoque <ahsan.ror@gmail.com>
2018-04-23 00:29:08 +0300

    primary landing and layout for add product done

--- app/src/main/java/com/example/ahsanulhoque/finoffer/landingpage/MainFinOffer.java
+++ app/src/main/java/com/example/ahsanulhoque/finoffer/landingpage/MainFinOffer.java
@@ -1,30 +1,73 @@
 package com.example.ahsanulhoque.finoffer.landingpage;
 
-import android.app.ActionBar;
 import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.text.Html;
-import android.view.Menu;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.support.v7.widget.Toolbar;
-import android.widget.Toast;
+import android.widget.ImageButton;
 
 import com.example.ahsanulhoque.finoffer.R;
 
 public class MainFinOffer extends AppCompatActivity {
     Toolbar lTopToolbar;
+    DrawerLayout mDrawerlayout;
+    ActionBarDrawerToggle mToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        updateStatusBarColor("#EF6C00");
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.main_fin_offer);
+        super.onCreate(savedInstanceState);
+//        statusbar color
+        updateStatusBarColor("#EF6C00");
+
+        //for drawer ode
+        mDrawerlayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerlayout, R.string.open, R.string.close);
+        mDrawerlayout.addDrawerListener(mToggle );
+        mToggle.syncState();
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //for custom toolbar
+        lTopToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(lTopToolbar);
+//        getSupportActionBar().setTitle("Fin Offer");
+
+//        recycler view
+        RecyclerView itemList = (RecyclerView) findViewById(R.id.itemlist);
+        itemList.setLayoutManager(new LinearLayoutManager(this));
+        String[] titles = {"Fresh Pizza", "Fresh Curry", "Fresh Burger", "C++", "Java", "Python", "Ruby"};
+        itemList.setAdapter(new ListAdapter(titles));
+
+        RecyclerView brandList = (RecyclerView) findViewById(R.id.brandList);
+        LinearLayoutManager brandLayoutManager =
+                new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        brandList.setLayoutManager(brandLayoutManager);
+        String[] heading = {"head1", "head2", "head3", "head4", "head5"};
+        brandList.setAdapter(new BrandAdapter(heading));
+
+//        for menu image click open drawer
+        ImageButton menuBTN =(ImageButton) findViewById(R.id.menuBTN);
+        menuBTN.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                    mDrawerlayout.openDrawer(Gravity.LEFT);
+
+
+
+            }
+        });
+
+
+
+       /* updateStatusBarColor("#EF6C00");
         lTopToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(lTopToolbar);
         getSupportActionBar().setTitle("Fin Offer");
@@ -39,13 +82,21 @@ public class MainFinOffer extends AppCompatActivity {
                 new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         brandList.setLayoutManager(brandLayoutManager);
         String[] heading = {"head1", "head2", "head3", "head4", "head5"};
-        brandList.setAdapter(new BrandAdapter(heading));
+        brandList.setAdapter(new BrandAdapter(heading));*/
 
 
 
     }
 
-    public void updateStatusBarColor(String color){// Color must be in hexadecimal fromat
+    /*@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }*/
+
+     public void updateStatusBarColor(String color){// Color must be in hexadecimal fromat
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getWindow();
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
