7232f4c6ebf6213a95a72dbec70b6155056e4e79
Md Ahsanul Hoque <ahsan.ror@gmail.com>
2018-04-23 15:50:54 +0300

    added add product page

--- app/src/main/java/com/example/ahsanulhoque/finoffer/AddProduct.java
+++ app/src/main/java/com/example/ahsanulhoque/finoffer/AddProduct.java
@@ -1,13 +1,56 @@
 package com.example.ahsanulhoque.finoffer;
 
+import android.graphics.Color;
+import android.os.Build;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageButton;
 
 public class AddProduct extends AppCompatActivity {
+
+    Toolbar lTopToolbar;
+    DrawerLayout pDrawerlayout;
+    ActionBarDrawerToggle mToggle;
+    NavigationView navigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_product);
+
+        updateStatusBarColor("#EF6C00");
+        //for drawer ode
+        pDrawerlayout = (DrawerLayout) findViewById(R.id.addp_drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this, pDrawerlayout, R.string.open, R.string.close);
+        pDrawerlayout.addDrawerListener(mToggle );
+        mToggle.syncState();
+        //for custom toolbar
+        lTopToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(lTopToolbar);
+
+        //        for menu image click open drawer
+        ImageButton menuBTN =(ImageButton) findViewById(R.id.menuBTN);
+        menuBTN.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                pDrawerlayout.openDrawer(Gravity.LEFT);
+            }
+        });
+    }
+
+    private void updateStatusBarColor(String color) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.parseColor(color));
+
+        }
     }
 }
--- app/src/main/java/com/example/ahsanulhoque/finoffer/landingpage/MainFinOffer.java
+++ app/src/main/java/com/example/ahsanulhoque/finoffer/landingpage/MainFinOffer.java
@@ -1,7 +1,11 @@
 package com.example.ahsanulhoque.finoffer.landingpage;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,19 +13,25 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Gravity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.support.v7.widget.Toolbar;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
+import com.example.ahsanulhoque.finoffer.AddProduct;
 import com.example.ahsanulhoque.finoffer.R;
 
 public class MainFinOffer extends AppCompatActivity {
     Toolbar lTopToolbar;
     DrawerLayout mDrawerlayout;
     ActionBarDrawerToggle mToggle;
+    NavigationView navigation;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.main_fin_offer);
@@ -54,16 +64,16 @@ public class MainFinOffer extends AppCompatActivity {
         String[] heading = {"head1", "head2", "head3", "head4", "head5"};
         brandList.setAdapter(new BrandAdapter(heading));
 
+
 //        for menu image click open drawer
         ImageButton menuBTN =(ImageButton) findViewById(R.id.menuBTN);
         menuBTN.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
                     mDrawerlayout.openDrawer(Gravity.LEFT);
-
-
-
             }
         });
+
+        initInstances();
 
 
 
@@ -85,18 +95,11 @@ public class MainFinOffer extends AppCompatActivity {
         brandList.setAdapter(new BrandAdapter(heading));*/
 
 
-
     }
 
-    /*@Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(mToggle.onOptionsItemSelected(item)){
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }*/
 
-     public void updateStatusBarColor(String color){// Color must be in hexadecimal fromat
+
+    public void updateStatusBarColor(String color){// Color must be in hexadecimal fromat
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getWindow();
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
@@ -105,4 +108,36 @@ public class MainFinOffer extends AppCompatActivity {
         }
     }
 
+    private void initInstances() {
+
+        navigation = (NavigationView) findViewById(R.id.navVW);
+        navigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                switch (id) {
+                    case R.id.addPRD:
+                        //Do some thing here
+                        // add navigation drawer item onclick method here
+                        Intent i = new Intent(MainFinOffer.this, AddProduct.class);
+                        startActivity(i);
+                        break;
+                    case R.id.dash:
+                        //Do some thing here
+                        // add navigation drawer item onclick method here
+                        Toast.makeText(MainFinOffer.this, "Hello", Toast.LENGTH_LONG).show();
+                        break;
+                }
+                return false;
+            }
+        });
+
+    }
+
+
 }
+
+
+
+
+
