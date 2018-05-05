3ecb50afda3d51fbd8e312239afa6362f6e6733d
jislam <engr.jislam@gmail.com>
2018-05-05 06:51:26 +0300

    progress bar integrated

--- app/src/main/java/com/example/ahsanulhoque/finoffer/activity/authentication/SignUp.java
+++ app/src/main/java/com/example/ahsanulhoque/finoffer/activity/authentication/SignUp.java
@@ -2,6 +2,7 @@ package com.example.ahsanulhoque.finoffer.activity.authentication;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -12,12 +13,12 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.ahsanulhoque.finoffer.R;
 import com.example.ahsanulhoque.finoffer.activity.core.MainFinOffer;
 import com.example.ahsanulhoque.finoffer.domain.UserProfile;
-import com.example.ahsanulhoque.finoffer.service.UserProfileService;
 import com.example.ahsanulhoque.finoffer.utility.FormUtility;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -25,6 +26,9 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Date;
 
@@ -41,7 +45,11 @@ public class SignUp extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
 
-    private UserProfileService userProfileService;
+    // progress bar
+    private Integer count;
+    private ProgressBar progressBar;
+
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("userProfile");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,8 +57,6 @@ public class SignUp extends AppCompatActivity {
         //getSupportActionBar().hide();
         updateStatusBarColor("#FFFFFF");
         setContentView(R.layout.signup);
-
-        userProfileService = new UserProfileService();
 
         firstNameET = (EditText) findViewById(R.id.firstnameET);
         lastNameET = (EditText) findViewById(R.id.lastnameET);
@@ -61,6 +67,10 @@ public class SignUp extends AppCompatActivity {
         signUpBTN = (Button) findViewById(R.id.signupBTN);
 
         firebaseAuth = FirebaseAuth.getInstance();
+
+        progressBar = (ProgressBar) findViewById(R.id.signUpProgressBar);
+        progressBar.setMax(100);
+        progressBar.setVisibility(View.INVISIBLE);
 
         signUpBTN.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -98,7 +108,7 @@ public class SignUp extends AppCompatActivity {
 
     private boolean validateForm() {
         boolean valid = true;
-
+        // email
         String email = emailET.getText().toString();
         if (TextUtils.isEmpty(email)) {
             emailET.setError("Required!");
@@ -109,7 +119,7 @@ public class SignUp extends AppCompatActivity {
         } else {
             emailET.setError(null);
         }
-
+        // password
         String password = passwordET.getText().toString();
         if (TextUtils.isEmpty(password)) {
             passwordET.setError("Required!");
@@ -120,7 +130,7 @@ public class SignUp extends AppCompatActivity {
         } else {
             passwordET.setError(null);
         }
-
+        // confirm password
         String confirmPassword = confirmPasswordET.getText().toString();
         if (TextUtils.isEmpty(confirmPassword)) {
             confirmPasswordET.setError("Required.");
@@ -148,19 +158,7 @@ public class SignUp extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
-                    FirebaseUser user = firebaseAuth.getCurrentUser();
-
-                    // fill userProfile info's
-                    UserProfile userProfile = new UserProfile();
-                    userProfile.setId(user.getUid());
-                    userProfile.setFirstName(firstNameET.getText().toString().trim());
-                    userProfile.setLastName(lastNameET.getText().toString().trim());
-                    userProfile.setEmail(email);
-                    userProfile.setCreated(new Date());
-                    userProfileService.createUserProfile(userProfile);
-
-                    updateUI(user);
-
+                    new UserProfileGeneratorTask().execute(email);
                 } else {
                     FirebaseAuthException e = (FirebaseAuthException) task.getException();
                     e.printStackTrace();
@@ -169,6 +167,67 @@ public class SignUp extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    public class UserProfileGeneratorTask extends AsyncTask<String, Integer, Void> {
+        boolean isAdded;
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            isAdded = false;
+            // progress bar
+            progressBar.setVisibility(View.VISIBLE);
+            progressBar.setProgress(0);
+        }
+
+        @Override
+        protected Void doInBackground(String... strings) {
+            String email = strings[0];
+            // fill userProfile info's
+            UserProfile userProfile = new UserProfile();
+            userProfile.setId(user.getUid());
+            userProfile.setFirstName(firstNameET.getText().toString().trim());
+            userProfile.setLastName(lastNameET.getText().toString().trim());
+            userProfile.setEmail(email);
+            userProfile.setCreated(new Date());
+            // create user profile
+            databaseReference.child(userProfile.getId()).setValue(userProfile, new DatabaseReference.CompletionListener() {
+                @Override
+                public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                    isAdded = true;
+                }
+            });
+            // progress bar
+            for (count = 1; count <= 5; count++) {
+                try {
+                    Thread.sleep(500);
+                    publishProgress(20*count);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            // progress bar
+            progressBar.setProgress(values[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            if (isAdded) {
+                isAdded = false;
+                // progress bar
+                progressBar.setVisibility(View.GONE);
+                updateUI(user);
+            }
+        }
     }
 
     /*private void checkPasswords() {
