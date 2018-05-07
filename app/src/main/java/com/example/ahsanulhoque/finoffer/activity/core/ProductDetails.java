0eebe8f2b15edf21f31e16b919ae02209ce659f3
Md Ahsanul Hoque <ahsan.ror@gmail.com>
2018-05-07 19:08:22 +0300

    added menu function in product details layout

--- app/src/main/java/com/example/ahsanulhoque/finoffer/activity/core/ProductDetails.java
+++ app/src/main/java/com/example/ahsanulhoque/finoffer/activity/core/ProductDetails.java
@@ -1,16 +1,34 @@
 package com.example.ahsanulhoque.finoffer.activity.core;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageButton;
 
 import com.example.ahsanulhoque.finoffer.R;
+import com.example.ahsanulhoque.finoffer.activity.authentication.LogInActivity;
+import com.example.ahsanulhoque.finoffer.activity.authentication.SingOutActivity;
 import com.example.ahsanulhoque.finoffer.domain.Product;
 
 public class ProductDetails extends AppCompatActivity {
+
+    private DrawerLayout mDrawerlayout;
+    private ActionBarDrawerToggle mToggle;
+    private NavigationView navigation;
+
+    private Toolbar lTopToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -20,6 +38,60 @@ public class ProductDetails extends AppCompatActivity {
         updateStatusBarColor("#EF6C00");
 
         Product product = (Product) getIntent().getSerializableExtra("product");
+
+
+        //for drawer ode
+        mDrawerlayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerlayout, R.string.open, R.string.close);
+        mDrawerlayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        //for custom toolbar
+        lTopToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(lTopToolbar);
+
+        // for menu image click open drawer
+        ImageButton menuBTN = (ImageButton) findViewById(R.id.menuBTN);
+        menuBTN.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mDrawerlayout.openDrawer(Gravity.LEFT);
+            }
+        });
+
+        initInstances();
+
+    }
+
+    private void initInstances() {
+        navigation = (NavigationView) findViewById(R.id.navVW);
+        navigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                switch (id) {
+                    case R.id.addPRD:
+                        //Do some thing here
+                        // add navigation drawer item onclick method here
+                        Intent i = new Intent(ProductDetails.this, ProductActivity.class);
+                        startActivity(i);
+                        mDrawerlayout.closeDrawer(Gravity.LEFT);
+                        break;
+                    case R.id.dash:
+                        Intent j = new Intent(ProductDetails.this, MainFinOffer.class);
+                        startActivity(j);
+                        mDrawerlayout.closeDrawer(Gravity.LEFT);
+                        break;
+                    case R.id.lgot:
+                        //Do some thing here
+                        // add navigation drawer item onclick method here
+                        new SingOutActivity().signOut();
+                        Intent intentMain = new Intent(ProductDetails.this, LogInActivity.class);
+                        startActivity(intentMain);
+                        break;
+                }
+                return false;
+            }
+        });
     }
 
     public void updateStatusBarColor(String color) {
