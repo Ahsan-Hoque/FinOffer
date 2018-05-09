321fff589a292f4ba9d1395eaf1188b502b4f108
jislam <engr.jislam@gmail.com>
2018-05-09 13:10:09 +0300

    Landing page (activity) can now firebase image

--- app/src/main/java/com/example/ahsanulhoque/finoffer/adapter/ListAdapter.java
+++ app/src/main/java/com/example/ahsanulhoque/finoffer/adapter/ListAdapter.java
@@ -1,6 +1,10 @@
 package com.example.ahsanulhoque.finoffer.adapter;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +14,7 @@ import android.widget.TextView;
 import com.example.ahsanulhoque.finoffer.R;
 import com.example.ahsanulhoque.finoffer.domain.Product;
 
+import java.io.InputStream;
 import java.util.List;
 
 
@@ -30,10 +35,15 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ItemListviewHo
     @Override
     public void onBindViewHolder(ItemListviewHolder holder, int position) {
         Product product = productList.get(position);
+
+        if (!TextUtils.isEmpty(product.getImageUrl())){
+            new DownloadImageTask(holder.image, 160, 120).execute(product.getImageUrl());
+        }
         String title = product.getName();
         String regPrice = String.valueOf(product.getRegularPrice());
         String newPrice = String.valueOf(product.getPrice());
         String location = product.getLocation();
+
         holder.title.setText(title);
         holder.regularPrice.setText("€" + regPrice);
         holder.price.setText("€" + newPrice);
@@ -59,6 +69,33 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ItemListviewHo
             location = (TextView) itemView.findViewById(R.id.locationTV);
             regularPrice = (TextView) itemView.findViewById(R.id.priceTV);
             price = (TextView) itemView.findViewById(R.id.newPriceTV);
+        }
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+        int height, width;
+
+        public DownloadImageTask(ImageView bmImage, int height, int width) {
+            this.bmImage = bmImage;
+            this.height = height;
+            this.width = width;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(Bitmap.createScaledBitmap(result, this.height, this.width, false));
         }
     }
 
